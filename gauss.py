8a3019ebc028f1d77608ef6f87ca6a9ca4861f6e
PauloVLB <vitorlimaborges@gmail.com>
2023-06-14 21:17:29 -0300

    fix refinamento

--- gauss.py
+++ gauss.py
@@ -32,7 +32,7 @@ def gauss(M, pivotamento = 0):
 
     return M
 
-def refinamento(M, x_barra):
+def refinamento(M, x_barra, pivotamento):
     # separa matriz A e vetor b
     A = np.copy(M[:, :-1])
     b = np.copy(M[:, -1])
@@ -40,11 +40,11 @@ def refinamento(M, x_barra):
     # calcula vetor residuo
     r = b - (A @ x_barra)
 
-    # adiciona vetor residuo como ultima coluna da matriz A
+    # adiciona vetor residuo como ultima coluna da matriz A 
     A = np.c_[A, r]
-    
-    # resolve sistema linear Ay = r
-    gauss(A, 0)
+        
+    # resolve sistema linear Ay = r para y
+    gauss(A, pivotamento)
     
     # vetor solucao do sistema linear
     y = np.copy(A[:, -1])
@@ -56,15 +56,11 @@ def refinamento(M, x_barra):
 
 
 arq = open(sys.argv[1])
-
 qnt_refinamentos = int(sys.argv[2])
-
 pivotamento = int(sys.argv[3])
 
 lines = arq.readlines()
-
 A = np.array([[float(x) for x in line.split(' ')] for line in lines[1:]])
-
 Acopy = np.copy(A)
 
 gauss(A, pivotamento)
@@ -76,16 +72,12 @@ print(x_barra)
 print("\n -------> Aplicando refinamento ", qnt_refinamentos, " vezes ", "com" if pivotamento == 1 else "sem", " pivotamento <-------")
 
 for i in range(qnt_refinamentos):
-    [x_barra, r] = refinamento(Acopy, x_barra)
+    [x_barra, r] = refinamento(Acopy, x_barra, pivotamento)
     print("\nNorma do vetor residuo ", i+1, ": ", np.linalg.norm(r))
-    
-    if(i > 0):
-        print("Diferença entre as normas dos vetores residuos ", i+1, " e ", i, ": ", np.linalg.norm(r) - np.linalg.norm(r_ant))
-    
-    r_ant = np.copy(r)
+  
     #print("Vetor solução após refinamento ", i+1, ": ")
     #print(x_barra)
 
-
-
+print("\nVetor solução final: ")
+print(x_barra)
 
